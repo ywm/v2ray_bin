f4e2493fe0b2456a09fceef6cdb3e1eda3a304d1
cary-sas <61653855+cary-sas@users.noreply.github.com>
2021-03-05 16:09:48 +0800

    v4.34.1

--- shadowsocks/scripts/ss_update.sh
+++ shadowsocks/scripts/ss_update.sh
@@ -8,6 +8,14 @@ alias echo_date='echo 【$(TZ=UTC-8 date -R +%Y年%m月%d日\ %X)】:'
 #main_url="https://raw.githubusercontent.com/hq450/fancyss/master/fancyss_arm"
 main_url="https://raw.githubusercontent.com/cary-sas/v2ray_bin/main/380_armv5_packge"
 backup_url=""
+socksopen_b=`netstat -nlp|grep -w 23456|grep -E "local|v2ray|xray"`
+if [ -n "$socksopen_b" ] && [ "$ss_basic_online_links_goss" == "1" ];then
+	echo_date "代理有开启，将使用代理网络..."
+	alias curlxx='curl --connect-timeout 8  --socks5-hostname 127.0.0.1:23456 '
+else
+	echo_date "使用常规网络下载..."
+	alias curlxx='curl --connect-timeout 8 '
+fi
 
 install_ss(){
 	echo_date 开始解压压缩包...
@@ -22,7 +30,7 @@ update_ss(){
 	echo_date 更新过程中请不要刷新本页面或者关闭路由等，不然可能导致问题！
 	echo_date 开启SS检查更新：使用主服务器：github
 	echo_date 检测主服务器在线版本号...
-	ss_basic_version_web1=`curl --connect-timeout 5 -s "$main_url"/latest.txt | sed -n 1p`
+	ss_basic_version_web1=`curlxx "$main_url"/latest.txt |  sed -n 1p`
 	if [ -n "$ss_basic_version_web1" ];then
 		echo_date 检测到主服务器在线版本号：$ss_basic_version_web1
 		dbus set ss_basic_version_web=$ss_basic_version_web1
@@ -32,7 +40,7 @@ update_ss(){
 			md5_web1=`curl -s "$main_url"/$ss_basic_version_web1/md5sum.txt | sed -n 1p`
 			echo_date 开启下载进程，从主服务器上下载更新包...
 			#wget --no-check-certificate --timeout=5 "$main_url"/$ss_basic_version_web1/shadowsocks.tar.gz
-			curl -L -H "Cache-Control: no-cache" -o /tmp/shadowsocks.tar.gz "$main_url"/$ss_basic_version_web1/shadowsocks.tar.gz
+			curlxx -o /tmp/shadowsocks.tar.gz "$main_url"/$ss_basic_version_web1/shadowsocks.tar.gz
 			md5sum_gz=`md5sum /tmp/shadowsocks.tar.gz | sed 's/ /\n/g'| sed -n 1p`
 			if [ "$md5sum_gz" != "$md5_web1" ]; then
 				echo_date 更新包md5校验不一致！估计是下载的时候出了什么状况，请等待一会儿再试...
